3e60af8f3de1a19adc23e0bd39a58e5424fbcd5c
Thadd Trinh <thadtrin@amazon.com>
2022-03-29 12:02:33 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { AmplifyMap, Geocoder } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure(awsExports);
+
+export default function MapWithGeocoder() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyMap>
+      <Geocoder />
+    </AmplifyMap>
   );
 }
-
-export default App;
